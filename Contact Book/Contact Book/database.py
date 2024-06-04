e0fc5b8de0380629c628433746803e572d6a5838
Shourya Sarkar <78168833+shouryasarkar@users.noreply.github.com>
2024-06-05 03:28:35 +0530

    Delete Contact Book directory

--- Contact Book/Contact Book/database.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-def create_connection():
-    return pymysql.connect(
-        host = "localhost",
-        user = "root",
-        password = "root123",
-        database = "contact_book"
-    )
